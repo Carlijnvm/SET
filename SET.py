b13c96b0a7868ce749fc055e324ec122607fb704
Carlijnvm <carlijnvm@icloud.com>
2025-01-17 12:14:08 +0100

    Update 1 SET.py
    Beginnetje SET.
    - Class Cards

--- SET.py
+++ SET.py
@@ -1,3 +1,23 @@
-import pygame
+class Card:
+    def __init__(self,symbol,number,color,shading):
+        self.symbol=symbol
+        self.number=number
+        self.color=color
+        self.shading=shading
 
-print('1')
+    def __str__(self):
+        return '%s, %s, %s, %s' % (Card.symbols[self.symbol],
+                           Card.numbers[self.number],
+                           Card.colors[self.color],
+                           Card.shadings[self.shading])
+
+    symbols=["diamond","squiggle","oval",]
+    numbers=["1","2","3"]
+    colors=["red","green","purple"]
+    shadings=["solid","striped","open"]
+
+    
+
+
+x = Card(1, 0, 1, 1)
+print((x))
